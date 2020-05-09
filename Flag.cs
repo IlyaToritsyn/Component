c23f7492cf5958f24b4dcdc38f34941a33d62887
IlyaToritsyn <toritsyn41@gmail.com>
2020-05-09 09:48:59 +0300

    Добавьте файлы проекта.

---
+++ Flag.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Компонент
+{
+    public partial class Flag : UserControl
+    {
+        Color[] mas = { Color.Blue, Color.White, Color.Red };
+        Color color1;
+        Color color2;
+        Color color3;
+
+        public Flag()
+        {
+            InitializeComponent();
+
+            color1 = mas[0];
+            color2 = mas[1];
+            color3 = mas[2];
+        }
+        
+
+        int count = 0;
+
+        public Color Color1
+        {
+            set { color1 = value; mas[0] = color1;  Refresh();  }
+            get { return color1; }
+        }
+
+        public Color Color2
+        {
+            set { color2 = value; mas[1] = color2; Refresh(); }
+            get { return color2; }
+        }
+
+        public Color Color3
+        {
+            set { color3 = value; mas[2] = color3; Refresh(); }
+            get { return color3; }
+        }
+
+
+        private void Flag_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics gr = CreateGraphics();
+            gr.FillRectangle(new SolidBrush(mas[ (count + 0) % 3 ]), 0, 0, this.Width/3, this.Height);
+            gr.FillRectangle(new SolidBrush(mas[ (count + 1) % 3 ]), this.Width / 3, 0, this.Width / 3*2, this.Height);
+            gr.FillRectangle(new SolidBrush(mas[ (count + 2) % 3 ]), this.Width / 3 * 2, 0, this.Width , this.Height);
+            gr.Dispose();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            count++;
+            Refresh();
+        }
+
+        private void Flag_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+        }
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Компонент
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.flag1 = new Компонент.Flag();
+            this.flag2 = new Компонент.Flag();
+            this.flag3 = new Компонент.Flag();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(416, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 74);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(393, 120);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // flag1
+            // 
+            this.flag1.Color1 = System.Drawing.Color.Blue;
+            this.flag1.Color2 = System.Drawing.Color.White;
+            this.flag1.Color3 = System.Drawing.Color.Red;
+            this.flag1.Location = new System.Drawing.Point(55, 12);
+            this.flag1.Name = "flag1";
+            this.flag1.Size = new System.Drawing.Size(294, 152);
+            this.flag1.TabIndex = 6;
+            // 
+            // flag2
+            // 
+            this.flag2.Color1 = System.Drawing.Color.Blue;
+            this.flag2.Color2 = System.Drawing.Color.White;
+            this.flag2.Color3 = System.Drawing.Color.Red;
+            this.flag2.Location = new System.Drawing.Point(102, 219);
+            this.flag2.Name = "flag2";
+            this.flag2.Size = new System.Drawing.Size(263, 160);
+            this.flag2.TabIndex = 7;
+            // 
+            // flag3
+            // 
+            this.flag3.Color1 = System.Drawing.Color.Blue;
+            this.flag3.Color2 = System.Drawing.Color.White;
+            this.flag3.Color3 = System.Drawing.Color.Red;
+            this.flag3.Location = new System.Drawing.Point(503, 248);
+            this.flag3.Name = "flag3";
+            this.flag3.Size = new System.Drawing.Size(304, 190);
+            this.flag3.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flag3);
+            this.Controls.Add(this.flag2);
+            this.Controls.Add(this.flag1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private Flag flag1;
+        private Flag flag2;
+        private Flag flag3;
+    }
+}
+
